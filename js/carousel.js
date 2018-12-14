d66245dbfea34b7a3d1a6abd86a66818c23dd977
tester <45738205+sergeilimbo@users.noreply.github.com>
2018-12-14 04:28:20 +0300

    Add files via upload

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,5 @@
+/*------Scroller--------*/
+
 var d = document;
 var wrap = d.querySelector('.wrap');
 var items = d.querySelector('.items');
@@ -19,3 +21,34 @@ function next() {
  setTransform();
 }
 window.addEventListener('resize', setTransform);﻿
+
+/*-------To-top-------*/
+ $(function() { 
+    $(window).scroll(function() { 
+    if($(this).scrollTop() != 0) { 
+    $('#toTop').fadeIn(); 
+    } else { 
+    $('#toTop').fadeOut();
+    }
+    }); 
+    $('#toTop').click(function() { 
+        $('body,html').animate({scrollTop:0},800); 
+        }); 
+        });
+ 
+ /*------Jakor-----*/
+ $(document).ready(function(){
+    $("#refOnButton").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
