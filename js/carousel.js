3e91ef7c10264c5c4a0f7585492a6ae37e01c217
tester <45738205+sergeilimbo@users.noreply.github.com>
2019-01-15 00:48:27 +0300

    Add files via upload

--- js/carousel.js
+++ js/carousel.js
@@ -52,3 +52,18 @@ window.addEventListener('resize', setTransform);﻿
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+$(document).ready(function(){
+    $("#refOnButton2").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
